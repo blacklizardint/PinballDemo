1864b8140465e3b7ddb094638af90c914bfe8ec3
Kevin Cherry <kcherr1@gmail.com>
2024-03-19 19:07:41 -0500

    wired in game over ui

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour {
     public GameObject mainMenu;
+    public GameObject gameoverMenu;
 
     void Start() {
         mainMenu.SetActive(true);
+        gameoverMenu.SetActive(false);
         Game.Instance.input.Disable();
     }
 
     public void StartGame() {
         mainMenu.SetActive(false);
+        Game.Instance.input.Enable();
+    }
+
+    public void GameOver() {
+        gameoverMenu.SetActive(true);
+        Game.Instance.input.Disable();
+    }
+
+    public void Restart() {
+        gameoverMenu.SetActive(false);
         Game.Instance.input.Enable();
     }
 }
